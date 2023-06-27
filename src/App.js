37820c8b88094ac57ece477f679ff1485ae4a4bf
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-06-27 22:32:46 +0530

    Added Route for UpdatePlace with dynamic routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation"
 import UserPlaces from "./places/pages/UserPalces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 function App() {
   return (
@@ -12,10 +13,16 @@ function App() {
       <main>
         <Routes>
           <Route path="/" Component={Users} />
-          <Route path="/places/new" Component={NewPlace} />
-          {/* in below path="/:userId/places" the ":userId" portion is dynami 
+          {/* in below path="/:userId/places" the ":userId" portion is dynami c
           and could be use for useParams hook */}
           <Route path="/:userId/places" Component={UserPlaces} />
+          <Route path="/places/new" Component={NewPlace} />
+          <Route path="/places/:placeId" Component={UpdatePlace} />
+          {/* Since both the above Route has path starting with "/places/" it could cause a problem 
+          if we write "/places/:placeId" this path before. This is beacuse :placeId is dynamic 
+          and can have any value even "new". So it is better to keep the path having "new" before 
+          the one having ":placeId". As this executes from top to bottom it checks new first the the 
+          dynamic routing */}
         </Routes>
       </main>
     </Router>
