df9b6b307870a406be0360cf340836d3e5323d0e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-20 16:12:05 +0530

    Custom hook:useForm to use in NewPlace UpdatePlace

---
+++ src/shared/hooks/form-hook.js
@@ -0,0 +1,45 @@
+//A custom hook is a function that starts with use and can share stateful logic
+
+//Here in form-hook I want to hold my useReducer logic that I've set up in NewPlace
+import { useCallback, useReducer } from "react";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useForm = (initialInputs, initialFormValidity) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValidity,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+  return [formState, inputHandler]
+};
