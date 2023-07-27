0b0778c8f84134ec17716844f38c606c67160b3a
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-27 23:17:31 +0530

    Listening context changes using useContext

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { NavLink } from 'react-router-dom'
 
+import { AuthContext } from '../../context/auth.context'
 import './NavLinks.css'
 
 function NavLinks() {
+  const auth= useContext(AuthContext)
   return (
     <ul className='nav-links'>
       <li><NavLink to='/' exact>ALL USERS</NavLink></li>
-      <li><NavLink to='/u1/places' >MY PLACES</NavLink></li>
-      <li><NavLink to='/places/new'>NEW PLACES</NavLink></li>
-      <li><NavLink to='/auth'>AUTHENTICATE </NavLink></li>
+      {auth.isLoggedIn && (<li><NavLink to='/u1/places' >MY PLACES</NavLink></li>)}
+      {auth.isLoggedIn && (<li><NavLink to='/places/new'>NEW PLACES</NavLink></li>)}
+      {!auth.isLoggedIn && (<li><NavLink to='/auth'>LOGIN </NavLink></li>)}
     </ul>
   )
 }
