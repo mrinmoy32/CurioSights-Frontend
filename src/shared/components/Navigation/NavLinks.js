8169f4bc5cdf585ce5d2e00e3e260ea079ef74b8
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-28 00:41:53 +0530

    Adds LOGOUT button in NavLinks

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -12,6 +12,7 @@ function NavLinks() {
       {auth.isLoggedIn && (<li><NavLink to='/u1/places' >MY PLACES</NavLink></li>)}
       {auth.isLoggedIn && (<li><NavLink to='/places/new'>NEW PLACES</NavLink></li>)}
       {!auth.isLoggedIn && (<li><NavLink to='/auth'>LOGIN </NavLink></li>)}
+      {auth.isLoggedIn && (<li><button onClick={auth.logout} >LOGOUT</button></li>)}
     </ul>
   )
 }
