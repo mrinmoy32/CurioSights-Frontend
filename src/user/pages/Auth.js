02afdc7e9fbd505f0966caf3af37f34b5e3b3cd5
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-27 21:52:07 +0530

    Adding SignUp + "SwitchMode Button" + name field

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "../../shared/hooks/form-hook";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_EMAIL,
+  VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
-import './Auth.css'
+import "./Auth.css";
 
 function Auth() {
-  const [formState, inputHandler] = useForm(
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: "",
@@ -28,35 +30,81 @@ function Auth() {
     console.log(formState.inputs); //send this to Backend when Backend is ready
   };
 
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          name: undefined,
+          ...formState.inputs,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          name: {
+            value: "",
+            isValid: false,
+          },
+          ...formState.inputs,
+        },
+        false
+      );
+    }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
   return (
-    <form className="authentication" onSubmit={AuthSubmitHandler}>
+    <React.Fragment>
+      <form className="authentication" onSubmit={AuthSubmitHandler}>
         <h4 className="authentication__header">Login Required!</h4>
         <hr />
-      <Input
-        id="email"
-        element="input"
-        type="email"
-        label="Email"
-        validators={[VALIDATOR_EMAIL()]}
-        errorText="Please enter a valid email id."
-        onInput={inputHandler}
-        placeholder="Please enter the email id"
-      />
-      <Input
-        id="password"
-        element="input"
-        type="password"
-        label="Password"
-        validators={[VALIDATOR_MINLENGTH(8)]}
-        errorText="Please enter a valid password (at least 8 characters)"
-        onInput={inputHandler}
-        placeholder="Please enter the password"
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        LOGIN
-      </Button>
-      <Button>SIGN UP</Button>
-    </form>
+        {!isLoginMode && (
+          <Input
+            id="name"
+            element="input"
+            type="name"
+            label="Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a name"
+            onInput={inputHandler}
+            placeholder="Please enter a name"
+          />
+        )}
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email id"
+          onInput={inputHandler}
+          placeholder="Please enter the email id"
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="Please enter a valid password (at least 8 characters)"
+          onInput={inputHandler}
+          placeholder="Please enter the password"
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoginMode ? "LOGIN" : "SIGN UP"}
+        </Button>
+        <div className="authentication__swich-button">
+          <Button
+            className="authentication__swich-button"
+            inverse
+            onClick={switchModeHandler}
+          >
+            {isLoginMode ? "New User? SIGN UP" : "Exixting User? LOGIN"}
+          </Button>
+        </div>
+      </form>
+    </React.Fragment>
   );
 }
 
