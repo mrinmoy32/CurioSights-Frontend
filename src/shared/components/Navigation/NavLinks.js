ec6e03c3ebb104fac2061e5d45f40019c8cdf478
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-24 02:57:15 +0530

    fix MyPlaces in NavLinks

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -9,7 +9,7 @@ function NavLinks() {
   return (
     <ul className='nav-links'>
       <li><NavLink to='/' exact>ALL USERS</NavLink></li>
-      {auth.isLoggedIn && (<li><NavLink to='/u1/places' >MY PLACES</NavLink></li>)}
+      {auth.isLoggedIn && (<li><NavLink to={`/${auth.userId}/places`} >MY PLACES</NavLink></li>)}
       {auth.isLoggedIn && (<li><NavLink to='/places/new'>NEW PLACES</NavLink></li>)}
       {!auth.isLoggedIn && (<li><NavLink to='/auth'>LOGIN </NavLink></li>)}
       {auth.isLoggedIn && (<li><button onClick={auth.logout} >LOGOUT</button></li>)}
