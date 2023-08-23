09f451aa00fc38615518857ad4dbdb7da4c6988b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-24 03:03:44 +0530

    only logged in user can edit/delete their place

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -83,10 +83,10 @@ function PlaceItem(props) {
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <Button to={`/places/${props.id}`}>EDIT</Button>
             )}
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <Button danger onClick={openConfirmDeleteModalHandler}>
                 DELETE
               </Button>
