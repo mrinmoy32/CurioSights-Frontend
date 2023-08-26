23227427a5eb2d3d52b7e30388b6b1f50e06707c
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-26 16:38:46 +0530

    adding correct user image path

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -11,7 +11,7 @@ function UserItem(props) {
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             {/* <img height={130} width={130} src={props.image} alt={props.name} /> */}
-            <Avatar image={props.image} alt={props.name} />
+            <Avatar image={`http://localhost:5000/${props.image}`} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
